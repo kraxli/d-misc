4295c45252c0089ab36d8ca98d7763f7fb3b5159
kraxli <kraxli77@gmail.com>
2018-05-23 15:57:43 +0200

    correct ending for dmd.conf and add some standard import paths

--- shell/include_d.sh
+++ shell/include_d.sh
@@ -8,15 +8,15 @@ sys.path.insert(0, '../src_py3')
 from dmisc import *
 
 # set your own path to dub
-dub_path =  r"/home/dave/.dub/packages/" 
+dub_path =  r"/home/dave/.dub/packages/"
 
 # set your own standard library imports
 std_imports = None
-
+std_imports = ['/usr/include/dmd/druntime/import', '/usr/include/dmd/phobos', '/snap/dmd/33/import/phobos']
 dmd_imports = generate_dmd_config(dub_path, std_imports)
 dcd_imports = generate_dcd_config(dub_path, std_imports)
 
-with open('../output/dmd.config', 'w') as file:
+with open('../output/dmd.conf', 'w') as file:
 	  file.write(dmd_imports)
 
 with open('../output/dcd.conf', 'w') as file:
